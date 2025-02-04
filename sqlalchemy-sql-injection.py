7fd04cb46cc7dbf31ae2feed8c1ae0bee8aa387a
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2025-02-04 10:04:54 +0100

    Update sqlalchemy-sql-injection.py

--- sqlalchemy-sql-injection.py
+++ sqlalchemy-sql-injection.py
@@ -1,3 +1,15 @@
 # ruleid: sqlalchemy-sql-injection
 def bad1(var):
     session.query(MyClass).distinct("foo={}".format(var))
+
+# ruleid: sqlalchemy-sql-injection
+def bad2(var):
+    query = cls.query.join(DeploymentPermission).having(
+        "oops{}".format(var)
+    )
+
+# ruleid: sqlalchemy-sql-injection
+def bad3(var):
+    query = cls.query.group_by(
+        "oops{}".format(var)
+    )
