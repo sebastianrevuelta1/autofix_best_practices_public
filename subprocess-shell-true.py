ac22365d15b224498c0206e6f1237e7b4349895a
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2025-02-04 10:02:52 +0100

    Create subprocess-shell-true.py

---
+++ subprocess-shell-true.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+# ok:subprocess-shell-true
+subprocess.call("echo 'hello'")
+
+# ok:subprocess-shell-true
+subprocess.call("grep -R {} .".format(sys.argv[1]))
+
+# ok:subprocess-shell-true
+subprocess.call("echo 'hello'", shell=True)
+
+# ruleid:subprocess-shell-true
+subprocess.call("grep -R {} .".format(sys.argv[1]), shell=True)
