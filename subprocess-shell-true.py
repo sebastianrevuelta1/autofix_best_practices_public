b76039a0faa1cbaa60f79b20e13812a08a79bbb4
sebasrevuelta <122784773+sebasrevuelta@users.noreply.github.com>
2025-02-04 10:05:25 +0100

    Update subprocess-shell-true.py

--- subprocess-shell-true.py
+++ subprocess-shell-true.py
@@ -12,3 +12,12 @@ subprocess.call("echo 'hello'", shell=True)
 
 # ruleid:subprocess-shell-true
 subprocess.call("grep -R {} .".format(sys.argv[1]), shell=True)
+
+# ruleid:subprocess-shell-true
+subprocess.call("grep -R {} .".format(sys.argv[1]), shell=True, cwd="/home/user")
+
+# ruleid:subprocess-shell-true
+subprocess.run("grep -R {} .".format(sys.argv[1]), shell=True)
+
+# ruleid:subprocess-shell-true
+subprocess.run("grep -R {} .".format(sys.argv[1]), shell=True)
